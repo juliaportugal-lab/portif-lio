2e3a40243d383f585de3eff9d08b6889667bcbb9
juliaportugal-lab <julia.portugal@escola.pr.gov.br>
2026-09-17 09:41:33 -0300

    Add files via upload

---
+++ atividade3/script.js
@@ -0,0 +1,10 @@
+let nota1trim;
+let mota2trim;
+let resultado;
+
+function calcular (){
+    nota1trim = Number(prompt ("Digite a nota do primeiro trimestre:"));
+    nota2trim = Number(prompt ("Digite a nota do segundo trimestre:"));
+
+    resultado = 180 - nota1trim + nota2trim
+}
